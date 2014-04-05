cd0a4b79569e622deea0b55b8a86ab80bcacaf71
Maxime FEBVAY <mfebvay@e1r4p14.42.fr>
2014-04-05 11:41:00 +0200

    index: indentation

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <html>
 	<head>
-	<link rel="stylesheet" type="text/css" href="css/styles.css" />
+		<link rel="stylesheet" type="text/css" href="css/styles.css" />
 	</head>
 	<body>
-<?php include "src/banner.php"; ?>
-<?php include "src/menu.php"; ?>
-<?php include "src/login.php"; ?>
-<?php include "src/content.php"; ?>
+		<?php include "src/banner.php"; ?>
+		<?php include "src/menu.php"; ?>
+		<?php include "src/login.php"; ?>
+		<?php include "src/content.php"; ?>
 	</body>
 </html>
