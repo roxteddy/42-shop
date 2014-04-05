47c2f62577a154cc1c25a86de755adfe457475b4
Maxime FEBVAY <mfebvay@e1r4p14.42.fr>
2014-04-05 16:45:40 +0200

    added menu items

--- src/menu.php
+++ src/menu.php
@@ -1,1 +1,6 @@
-<div id="menu">menu</div>
+<div id="menu">
+<a href="index.php?page=shop&cat=action"><div class="cat">Action</div></a>
+<a href="index.php?page=shop&cat=adventure"><div class="cat">Adventure</div></a>
+<a href="index.php?page=shop&cat=puzzle"><div class="cat">Puzzle</div></a>
+<a href="index.php?page=shop&cat=strategie"><div class="cat">Strategie</div></a>
+</div>
