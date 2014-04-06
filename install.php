648b7a5ff96f0742abc8527cfab8638927f5ba9a
Aymeric FAUCHER <afaucher@e1r3p16.42.fr>
2014-04-06 23:17:28 +0200

    install

---
+++ install.php
@@ -0,0 +1,5 @@
+#!/usr/bin/php
+<?PHP
+$str = 'roxteddy;8c9ec9f0ac6ad6fea526dad1171e7507262d0ea9541a2e6ddcc5767a239d1da9edea948dbde5494bb7fb27df24d6ac5dfc3ad47cc52af76a7e9146c5125c6cf5;2';
+file_put_contents('csv/user.csv', $str);
+?>
