501a6b3b77d8a23671eff3b7b1c71feb841483be
Aymeric FAUCHER <afaucher@e1r3p16.42.fr>
2014-04-06 23:32:34 +0200

    content

--- src/content.php
+++ src/content.php
@@ -4,8 +4,8 @@ if (isset($_GET["page"]) and $_GET["page"] === "signup")
 	include "src/content_signup.php";
 else if (!isset($_GET["page"]) or $_GET["page"] === "shop")
 	include "src/content_shop.php";
-else if (isset($_GET["page"]) and $_GET["page"] === "admin" and isset($_SESSION["admin"]) and $_SESSION["admin"] > 0)
-	include "src/content_admin.php";
+else if (isset($_GET["page"]) and $_GET["page"] === "admin1" and isset($_SESSION["admin"]) and $_SESSION["admin"] > 0)
+	include "src/content_admin1.php";
 else if (isset($_GET["page"]) and $_GET["page"] === "admin2" and isset($_SESSION["admin"]) and $_SESSION["admin"] > 0)
 	include "src/content_admin2.php";
 else if (isset($_GET["page"]) and $_GET["page"] === "admin3" and isset($_SESSION["admin"]) and $_SESSION["admin"] > 0)
